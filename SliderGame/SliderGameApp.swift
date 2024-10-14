212a23fa503ba3b33aff502041ce770cb9087df9
Chron1um635 <chron1um522@gmail.com>
2024-10-14 11:57:21 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Максим Назаров on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
