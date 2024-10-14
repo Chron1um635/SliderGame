30d16cf1254c4db0692c634bdfbf88af9dafb4b2
Chron1um635 <chron1um522@gmail.com>
2024-10-14 12:45:56 +0300

    setup view model

--- SliderGame/ContentView/ContentViewViewModel.swift
+++ SliderGame/ContentView/ContentViewViewModel.swift
@@ -1,8 +1,32 @@
 //
-//  ContentViewVIewModel.swift
+//  ContentViewViewModel.swift
 //  SliderGame
 //
 //  Created by Максим Назаров on 14.10.2024.
 //
 
 import Foundation
+
+
+@Observable
+final class ContentViewViewModel {
+    var game = Game()
+    var isActive = false
+    
+    var score: Int {
+        100 - abs(game.targetValue - lround(game.currentValue))
+    }
+    
+    var alpha: Double {
+        Double(score) / 100
+    }
+    
+    func showAlert() {
+        isActive.toggle()
+    }
+    
+    func reset() {
+        game.targetValue = Int.random(in: 0...100)
+        game.currentValue = Double.random(in: 0...100)
+    }
+}
